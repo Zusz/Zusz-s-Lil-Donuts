d11a9b7971a5d465c656a10574d3936a62bde594
Zusz <zusz2011@gmail.com>
2025-10-20 18:37:51 +0200

    removed remainder of config

--- src/main/java/net/zusz/zdonutmod/ZDonutMod.java
+++ src/main/java/net/zusz/zdonutmod/ZDonutMod.java
@@ -34,9 +34,6 @@ public class ZDonutMod {
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
-
-        // Register our mod's ModConfigSpec so that FML can create and load the config file for us
-        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
     private void commonSetup(FMLCommonSetupEvent event) {
